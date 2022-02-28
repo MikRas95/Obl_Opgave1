f5792c53180f885c8223e344f040a515cd0e2aad
Mikkel Krogsgaard Rasmussen <mik.ras@hotmail.com>
2022-02-28 13:07:31 +0100

    Add project files.

---
+++ Obl_Opgave1/Car.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Obl_Opgave1
+{
+    public class Car
+    {
+        private int _id;
+        private string _model;
+        private double _price;
+        private string _licensePlate;
+
+        // LAV ID / Skal der laves en test på id???????
+        //public int Id
+        //{
+        //    get => _id;
+        //    set
+        //    {
+
+        //    }
+        //}
+
+        public string Model
+        {
+            get => _model;
+            set
+            {
+                if (value == null) throw new ArgumentNullException("model", "Model must not be null");
+                if (value.Length < 4) throw new ArgumentException("Model must have at least 4 characters");
+                _model = value;
+            }
+        }
+
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (value == 0) throw new ArgumentNullException("price", "Price must not be 0");
+                if (value < 0) throw new ArgumentOutOfRangeException("price", "Price must be higher than 0");
+                _price = value;
+            } 
+        }
+
+        public string LicensePlate
+        {
+            get => _licensePlate;
+            set
+            {
+                if (value == null) throw new ArgumentNullException("LicensePlate", "LicensePlate must not be null");
+                if (value.Length < 2 || value.Length > 7) throw new ArgumentException("LicensePlate length must be between 2 and 7 characters");
+                _licensePlate = value;
+            }
+        }
+
+        public Car()
+        {
+
+        }
+        public Car(string model, double price, string licensePlate)
+        {
+            Model = model;
+            Price = price;
+            LicensePlate = licensePlate;
+        }
+    }
+
+}
---
+++ Obl_Opgave1Tests/CarTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Obl_Opgave1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obl_Opgave1.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        private Car _car;
+        [TestInitialize]
+        public void Init()
+        {
+            _car = new Car("Tesla",1000000,"ABC");
+        }
+        [TestMethod()]
+        public void TestConstructor()
+        {
+            Assert.AreEqual("Tesla", _car.Model);
+            Assert.AreEqual(1000000, _car.Price);
+            Assert.AreEqual("ABC", _car.LicensePlate);
+        }
+
+        [TestMethod]
+        public void TestModel()
+        { 
+            Assert.AreEqual("Tesla",_car.Model);
+            Assert.ThrowsException<ArgumentNullException>(() => _car.Model = null);
+            Assert.ThrowsException<ArgumentException>(() => _car.Model = "MKL");
+        }
+
+        [TestMethod]
+        public void TestPrice()
+        {
+            Assert.AreEqual(1000000, _car.Price);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.Price = -1);
+            Assert.ThrowsException<ArgumentNullException>(() => _car.Price = 0);
+        }
+
+        [TestMethod]
+        public void TestLicensePlate()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => _car.LicensePlate = null);
+            Assert.ThrowsException<ArgumentException>(() => _car.LicensePlate = "F");
+            Assert.ThrowsException<ArgumentException>(() => _car.LicensePlate = "QWERTYUI");
+        }
+    }
+}
